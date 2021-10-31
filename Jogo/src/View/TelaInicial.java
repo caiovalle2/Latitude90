17c011b3aa82d27e8aa9ae247e0f4d5ffe8e2af7
caiovalle2 <caiovalle1999@hotmail.com>
2021-10-31 14:00:52 -0300

    Create TelaInicial.java

---
+++ Jogo/src/View/TelaInicial.java
@@ -0,0 +1,27 @@
+package View;
+import java.awt.*;
+import javax.swing.*;
+
+public class TelaInicial extends JFrame{
+	public final int LARG_DEFAULT=800;
+	public final int ALT_DEFAULT=400;
+	JPanel p;
+	
+	public TelaInicial() {
+		JButton carregar = new JButton("Carregar partida");
+		carregar.setBounds(300, 280, 200, 50);
+		add(carregar);
+		PainelInicial inicial = new PainelInicial();
+		inicial.addMouseListener(new EventTelaInicial(this));
+		getContentPane().add(inicial);
+		Toolkit tk=Toolkit.getDefaultToolkit();
+		Dimension screenSize=tk.getScreenSize();
+		int sl=screenSize.width;
+		int sa=screenSize.height;
+		int x=sl/2-LARG_DEFAULT/2;
+		int y=sa/2-ALT_DEFAULT/2;
+		
+		setBounds(x,y,LARG_DEFAULT,ALT_DEFAULT);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+}
