9d27dabca4da96b6c97ed452f2b6ad2ad21e4ee5
caiovalle2 <caiovalle1999@hotmail.com>
2021-10-31 13:01:32 -0300

    Update Frame.java

--- Jogo/src/View/Frame.java
+++ Jogo/src/View/Frame.java
@@ -1,12 +1,9 @@
 package View;
 import java.awt.*;
-import java.io.*;
 
 import javax.swing.*;
 
 import Model.*;
-
-import java.awt.*;
 public class Frame extends JFrame {
 	public final int LARG_DEFAULT=1200;
 	public final int ALT_DEFAULT=750;
@@ -15,16 +12,21 @@ public class Frame extends JFrame {
 	public Frame(Regras regras) {
 		JButton dados = new JButton("Jogar Dados");
 		dados.setBounds(800,120,200,50);
+		dados.addActionListener(new Jogardados(regras,this));
 		add(dados);
 		Toolkit tk=Toolkit.getDefaultToolkit();
+		
 		Dimension screenSize=tk.getScreenSize();
 		int sl=screenSize.width;
 		int sa=screenSize.height;
 		int x=sl/2-LARG_DEFAULT/2;
 		int y=sa/2-ALT_DEFAULT/2;
-		p = new Painel();
+		
+		/*Painel do jogo*/
+		p = new Painel(regras);
 		p.addMouseListener(new EventMov());
 		getContentPane().add(p);
+		
 		setBounds(x,y,LARG_DEFAULT,ALT_DEFAULT);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
