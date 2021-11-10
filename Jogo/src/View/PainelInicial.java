5bb10123789f911e5d4c7af19a145271ab87d7f7
caiovalle2 <caiovalle1999@hotmail.com>
2021-11-10 14:04:50 -0300

    Update PainelInicial.java

--- Jogo/src/View/PainelInicial.java
+++ Jogo/src/View/PainelInicial.java
@@ -3,21 +3,32 @@ package View;
 import java.awt.*;
 import javax.swing.*;
 
-public class PainelInicial extends JPanel{
-
+public class PainelInicial extends JPanel{  
 	
 	public PainelInicial() {
 
 	}
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
 		
-		g.drawRect(300, 100, 200, 50);
-		g.drawString("Jogo - 1 x 1", 360, 130);
-		g.drawRect(300, 160, 200, 50);
-		g.drawString("Jogo - 4 individual", 360, 190);
-		g.drawRect(300, 220, 200, 50);
-		g.drawString("Jogo - 4 dupla", 360, 250);
+		g2d.drawRect(300, 60, 200, 50);
+		g2d.setPaint(Color.LIGHT_GRAY);
+		g2d.fillRect(300,60,200,50);
+		g2d.setPaint(Color.BLACK);
+		g2d.drawString("Jogo - 1 x 1", 360, 90);
+		
+		g2d.drawRect(300, 120, 200, 50);
+		g2d.setPaint(Color.LIGHT_GRAY);
+		g2d.fillRect(300,120,200,50);
+		g2d.setPaint(Color.BLACK);
+		g2d.drawString("Jogo - 4 individual", 360, 150);
+
+		g2d.drawRect(300, 180, 200, 50);
+		g2d.setPaint(Color.LIGHT_GRAY);
+		g2d.fillRect(300,180,200,50);
+		g2d.setPaint(Color.BLACK);
+		g2d.drawString("Jogo - 4 dupla", 360, 210);
 		
 		
 	}
