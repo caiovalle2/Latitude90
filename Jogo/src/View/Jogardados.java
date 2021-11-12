d162d93b6be724c39cb30a79967ee47d9068dd1c
caiovalle2 <caiovalle1999@hotmail.com>
2021-11-12 12:14:15 -0300

    Update Jogardados.java

--- Jogo/src/View/Jogardados.java
+++ Jogo/src/View/Jogardados.java
@@ -8,20 +8,25 @@ public class Jogardados implements  ActionListener{
 	Regras regras;
 	Dados imgdados;
 	Frame f;
-	boolean estado = true;
+	static boolean estado = true;
 	public Jogardados(Regras regras, Frame f) {
 		this.regras = regras;
 		this.f = f;
 	}
 	public void actionPerformed(ActionEvent e) {
-		
+		int dados[] = {0,0};
 		
 		if(estado) {
-			int dados[] = {0,0};
+			
 			dados = regras.jogardado();
 			/*Exibir os dados*/
 			f.repaint();
 			estado = false;
+		}
+		
+		if(dados[0] == dados[1]) {
+			char colorido = regras.dadocolorido();
+			
 		}
 		
 		
