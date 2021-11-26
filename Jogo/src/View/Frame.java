8b1f975736956d7b450f3f6e0aa3194009a6672a
caiovalle2 <caiovalle1999@hotmail.com>
2021-11-26 17:52:01 -0300

    Update Frame.java

--- Jogo/src/View/Frame.java
+++ Jogo/src/View/Frame.java
@@ -8,14 +8,18 @@ public class Frame extends JFrame {
 	public final int LARG_DEFAULT=1200;
 	public final int ALT_DEFAULT=750;
 	JPanel p;
-	
+	static JLabel info = new JLabel("Inicio de jogo");
 	public Frame(Regras regras) {
 		JButton dados = new JButton("Jogar Dados");
-		dados.setBounds(850,200,200,50);
+		dados.setBounds(850,300,200,50);
 		dados.addActionListener(new Jogardados(regras,this));
 		add(dados);
-		Toolkit tk=Toolkit.getDefaultToolkit();
 		
+		info.setBounds(900,190,200,50);
+		info.setFont(new Font("Arial",Font.BOLD,20));
+		add(info);
+		
+		Toolkit tk=Toolkit.getDefaultToolkit();
 		Dimension screenSize=tk.getScreenSize();
 		int sl=screenSize.width;
 		int sa=screenSize.height;
