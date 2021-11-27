7da51c7b6a0a029cf1c676cdf917b64ed16dee36
caiovalle2 <caiovalle1999@hotmail.com>
2021-11-27 12:41:00 -0300

    Update Frame.java

--- Jogo/src/View/Frame.java
+++ Jogo/src/View/Frame.java
@@ -2,7 +2,7 @@ package View;
 import java.awt.*;
 
 import javax.swing.*;
-
+import java.awt.event.*;
 import Model.*;
 public class Frame extends JFrame {
 	public final int LARG_DEFAULT=1200;
@@ -19,6 +19,67 @@ public class Frame extends JFrame {
 		info.setFont(new Font("Arial",Font.BOLD,20));
 		add(info);
 		
+		String dado[] = {"1","2","3","4","5","6"};
+		JButton simular = new JButton("Simular");
+		JComboBox dado1 = new JComboBox(dado);
+		JComboBox dado2 = new JComboBox(dado);
+		dado1.setBounds(750,520,110,20);
+		dado2.setBounds(900,520,110,20);
+		simular.setBounds(1050,520,110,20);
+		simular.addActionListener(new ActionListener() {
+			int soma[] = {0,0,0,0}, i = 0;
+			public void actionPerformed(ActionEvent e) {
+				if(regras.colorido) {
+					regras.dadocolorido();
+					regras.colorido = false;
+					regras.acaocor = true;
+				}
+				else {
+					if(!regras.ord_ind) {
+						regras.set_dados(dado1.getSelectedIndex()+1,dado2.getSelectedIndex()+1);
+						soma[i] = regras.dados[0] + regras.dados[1];
+						i++;
+						if(i == regras.qnt) {
+							for(int x = 0; x < i; x++) {
+								int n = 0;
+								int val = 0, val2 = 0;
+								for(int y = 0; y < i - x - 1; y++) {
+									if(soma[y] < soma[y+1]) {
+										n = y;
+									}
+									else {
+										n = y+1;
+									}
+								}
+								val = soma[i-x-1];
+								soma[i-x-1] = soma[n];
+								soma[n] = val;
+								val2 = regras.indice[i-x-1];
+								regras.indice[i-x-1] = regras.indice[n];
+								regras.indice[n] = val2;
+							}
+							regras.ord_ind = true;
+							System.out.println(i + " "+ regras.indice[0] + " " + regras.indice[1]);
+						}
+					}
+					else if(regras.estado) {
+						regras.set_dados(dado1.getSelectedIndex()+1,dado2.getSelectedIndex()+1);
+						/*Exibir os dados*/
+						regras.estado = false;
+						regras.iniciar = true;
+					}
+		
+					if(regras.dados[0] == regras.dados[1] && regras.iniciar) {
+						regras.colorido = true;
+						regras.iniciar = false;
+					}
+				}
+				repaint();
+			}
+		});
+		add(dado1);
+		add(dado2);
+		add(simular);
 		Toolkit tk=Toolkit.getDefaultToolkit();
 		Dimension screenSize=tk.getScreenSize();
 		int sl=screenSize.width;
