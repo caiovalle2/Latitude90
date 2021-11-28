47e946ec7ff6867ae05b026f6e7f3d6ba4a657ba
caiovalle2 <caiovalle1999@hotmail.com>
2021-11-27 23:02:09 -0300

    Update Frame.java

--- Jogo/src/View/Frame.java
+++ Jogo/src/View/Frame.java
@@ -4,77 +4,39 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import Model.*;
+import controller.*;
 public class Frame extends JFrame {
 	public final int LARG_DEFAULT=1200;
 	public final int ALT_DEFAULT=750;
 	JPanel p;
 	public Frame(Regras regras) {
 		JButton dados = new JButton("Jogar Dados");
-		dados.setBounds(850,300,200,50);
-		dados.addActionListener(new Jogardados(regras,this));
+		dados.setBounds(850,350,200,50);
+		dados.addActionListener(new JogarDados(regras,this));
 		add(dados);
 		
 		String dado[] = {"1","2","3","4","5","6"};
 		JButton simular = new JButton("Simular");
 		JComboBox dado1 = new JComboBox(dado);
 		JComboBox dado2 = new JComboBox(dado);
-		dado1.setBounds(750,520,110,20);
-		dado2.setBounds(900,520,110,20);
-		simular.setBounds(1050,520,110,20);
-		simular.addActionListener(new ActionListener() {
-			int soma[] = {0,0,0,0}, i = 0;
-			public void actionPerformed(ActionEvent e) {
-				if(regras.colorido) {
-					regras.dadocolorido();
-					regras.colorido = false;
-					regras.acaocor = true;
-				}
-				else {
-					if(!regras.ord_ind) {
-						regras.set_dados(dado1.getSelectedIndex()+1,dado2.getSelectedIndex()+1);
-						soma[i] = regras.dados[0] + regras.dados[1];
-						i++;
-						if(i == regras.qnt) {
-							for(int x = 0; x < i; x++) {
-								int n = 0;
-								int val = 0, val2 = 0;
-								for(int y = 0; y < i - x - 1; y++) {
-									if(soma[y] < soma[y+1]) {
-										n = y;
-									}
-									else {
-										n = y+1;
-									}
-								}
-								val = soma[i-x-1];
-								soma[i-x-1] = soma[n];
-								soma[n] = val;
-								val2 = regras.indice[i-x-1];
-								regras.indice[i-x-1] = regras.indice[n];
-								regras.indice[n] = val2;
-							}
-							regras.ord_ind = true;
-							System.out.println(i + " "+ regras.indice[0] + " " + regras.indice[1]);
-						}
-					}
-					else if(regras.estado) {
-						regras.set_dados(dado1.getSelectedIndex()+1,dado2.getSelectedIndex()+1);
-						/*Exibir os dados*/
-						regras.estado = false;
-						regras.iniciar = true;
-					}
-		
-					if(regras.dados[0] == regras.dados[1] && regras.iniciar) {
-						regras.colorido = true;
-						regras.iniciar = false;
-					}
-				}
-				repaint();
-			}
-		});
+		dado1.setBounds(750,570,110,20);
+		dado2.setBounds(900,570,110,20);
+		simular.setBounds(1050,570,110,20);
+		simular.addActionListener(new SimularDados(this,regras, dado1, dado2));
 		add(dado1);
 		add(dado2);
 		add(simular);
+		
+		JMenuBar mb = new JMenuBar();
+		JMenu menu = new JMenu("Menu");
+		JMenuItem novo =  new JMenuItem("Novo jogo"), salvar = new JMenuItem("Salvar");
+		menu.add(novo);
+		menu.add(salvar);
+		mb.add(menu);
+		novo.addActionListener(new NovoJogo(this));			
+		salvar.addActionListener(new Salvar(regras));
+		setJMenuBar(mb);
+		
 		Toolkit tk=Toolkit.getDefaultToolkit();
 		Dimension screenSize=tk.getScreenSize();
 		int sl=screenSize.width;
