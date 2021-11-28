97ee5c4109f8ffa0fc27288b12e6edc98e002e1b
caiovalle2 <caiovalle1999@hotmail.com>
2021-11-27 23:01:50 -0300

    Create Salvar.java

---
+++ Jogo/src/controller/Salvar.java
@@ -0,0 +1,12 @@
+package controller;
+import java.awt.event.*;
+import Model.*;
+public class Salvar implements ActionListener {
+	Regras regras;
+	public Salvar(Regras r){
+		regras = r;
+	}
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
